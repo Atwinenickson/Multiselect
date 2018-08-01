40644b94106e6d7f0868a54e0e684091abc232e7
pbwauyo <pbwauyo@gmail.com>
2018-08-01 17:17:49 +0300

    current changes

--- src/components/multiselectContainer.ts
+++ src/components/multiselectContainer.ts
@@ -17,37 +17,48 @@ export interface ContainerProps extends WrapperProps {
     displayAttr: string;
     fieldCaption: string;
     constraint: string;
-    sortAttribute: string;
-    labelAttribute: string;
     sortOrder: string;
-    showLabel: string;
 }
 
 export interface ContainerState {
     checkboxItems: { guid: string, caption: string, isChecked: boolean }[];
 }
-
+let expanded = false;
 export default class MultiselectContainer extends Component<ContainerProps, ContainerState> {
     readonly state: ContainerState = {
         checkboxItems: []
     };
-
     private reference: string;
     private entity: string;
-    // private captionNode: any;
+    private Node: HTMLDivElement;
 
     constructor(props: ContainerProps) {
         super(props);
         this.entity = this.props.entity1.split("/")[1];
         this.reference = this.props.entity1.split("/")[0];
         this.getDataFromXPath = this.getDataFromXPath.bind(this);
+
     }
     render() {
         return (
-            createElement("div", { className: "multi" },
+            createElement("div", { className: "multiselect" },
+                    createElement("div",
+                        {
+                            className: "selectBox",
+                            onClick: this.showCheckboxes
+                        },
+                        createElement("select", { },
+                            createElement("option", {}, "Select from these options available")),
+                        createElement("div", {
+                            className: "overSelect"
+                        })),
+                    createElement("div", {
+                        ref: this.setReference,
+                        id: "checkboxes"
+                    },
             this.createCheckboxItems()
         )
-        );
+        ));
     }
     componentWillReceiveProps(newProps: ContainerProps) {
         if (newProps.mxObject !== this.props.mxObject) {
@@ -74,12 +85,11 @@ export default class MultiselectContainer extends Component<ContainerProps, Cont
     }
 
     private processCheckboxItems = (multiSelectObjects: mendix.lib.MxObject[]) => {
-
         const referencedObjects = this.props.mxObject.getReferences(this.reference);
 
         const checkboxItems = multiSelectObjects.map(mxObj => {
             const guid = mxObj.getGuid();
-            const caption = mxObj.get(this.props.labelAttribute) as string;
+            const caption = mxObj.get(this.props.displayAttr) as string;
             const isChecked = referencedObjects.indexOf(guid) > -1;
 
             return {
@@ -94,32 +104,35 @@ export default class MultiselectContainer extends Component<ContainerProps, Cont
     private createCheckboxItems() {
         const checkboxarray: any = [];
         this.state.checkboxItems.map(item => {
-            checkboxarray.push(
-                createElement("div", { className: "multiselect" },
-                    createElement("div",
-                        {
-                            className: "selectBox"
-                        },
-                        createElement("select", {},
-                            createElement("option", {}, "Select any language")),
-                        createElement("div", {
-                            className: "overSelect"
-                        })),
-                    createElement("div", {
-                        id: "checkboxes"
-                    },
+            checkboxarray.push([
                         createElement("label", {
                             className: "myClassName"
-                        }, createElement("input", {
+                        }),
+                        createElement("input", {
                             type: "checkbox",
                             isChecked: item.isChecked,
                             key: item.guid
-                        })
-                        ), item.caption
-                    ))
-            );
-        });
+                        }), item.caption ]
+                    );
+                });
 
         return checkboxarray;
     }
+
+    private setReference = (Node: HTMLDivElement) => {
+        this.Node = Node;
+    }
+
+    private showCheckboxes = () => {
+         const checkboxes = this.Node;
+         if (!expanded) {
+          checkboxes.style.display = "block";
+          expanded = true;
+        } else {
+          checkboxes.style.display = "none";
+          expanded = false;
+        }
+        // tslint:disable-next-line:no-console
+       // console.log(this.Node);
+      }
 }
