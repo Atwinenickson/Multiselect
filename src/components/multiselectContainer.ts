144f1ba238555a6845b31777544135705e07b44f
atwinenickson <atwiinenicxon@gmail.com>
2018-08-07 17:02:13 +0300

    Updated Multiselect Widget

--- src/components/multiselectContainer.ts
+++ src/components/multiselectContainer.ts
@@ -18,41 +18,54 @@ export interface ContainerProps extends WrapperProps {
     fieldCaption: string;
     constraint: string;
     sortOrder: string;
+    showLabel: string;
 }
 
 export interface ContainerState {
     checkboxItems: { guid: string, caption: string, isChecked: boolean }[];
     isChecked: boolean;
+    fieldCaption?: string;
+    value?: string;
+    caption?: string;
+    names: any [];
 }
 let expanded = false;
 export default class MultiselectContainer extends Component<ContainerProps, ContainerState> {
-    private subscriptionHandles: number[] = [];
-    readonly state: ContainerState = {
+   private subscriptionHandles: number[] = [];
+   readonly state: ContainerState = {
         checkboxItems: [],
-        isChecked: false
+        isChecked: false,
+        names: [],
+        fieldCaption: this.props.mxObject
+        ? this.props.mxObject.get(this.props.entity1) as string
+        : ""
     };
-    private reference: string;
-    private entity: string;
-    private Node: HTMLDivElement;
+   private reference: string;
+   private entity: string;
+   private Node: HTMLDivElement;
+   private myarray: string[] = [];
+   fieldCaption: string;
 
-    constructor(props: ContainerProps) {
+   constructor(props: ContainerProps) {
         super(props);
         this.entity = this.props.entity1.split("/")[1];
         this.reference = this.props.entity1.split("/")[0];
         this.getDataFromXPath = this.getDataFromXPath.bind(this);
         this.handelChange = this.handelChange.bind(this);
-
+        this.getDataFromMicroflow = this.getDataFromMicroflow.bind(this);
     }
-    render() {
+   render() {
+        const name = this.state.names.join(",");
         return (
-            createElement("div", { className: "multiselect" },
+            createElement("legend", {}, this.props.fieldCaption,
+                createElement("div", { className: "multiselect" },
                     createElement("div",
                         {
                             className: "selectBox",
                             onClick: this.showCheckboxes
                         },
-                        createElement("select", { },
-                            createElement("option", {}, "Select from these options available")),
+                        createElement("select", { id: "select" },
+                            createElement("option", {}, name)),
                         createElement("div", {
                             className: "overSelect"
                         })),
@@ -60,11 +73,12 @@ export default class MultiselectContainer extends Component<ContainerProps, Cont
                         ref: this.setReference,
                         id: "checkboxes"
                     },
-            this.createCheckboxItems()
-        )
-        ));
+                        this.props.fieldCaption,
+                        this.createCheckboxItems()
+                    )
+                )));
     }
-    componentWillReceiveProps(newProps: ContainerProps) {
+   componentWillReceiveProps(newProps: ContainerProps) {
         if (newProps.mxObject !== this.props.mxObject) {
             this.getDataFromXPath(newProps.mxObject);
             this.resetSubscriptions(newProps.mxObject);
@@ -73,15 +87,15 @@ export default class MultiselectContainer extends Component<ContainerProps, Cont
         }
     }
 
-    private getDataFromXPath = (mxObject: mendix.lib.MxObject) => {
+   private getDataFromXPath = (mxObject: mendix.lib.MxObject) => {
         if (mxObject) {
-             const constraint = this.props.constraint;
-             const xpath = "//" + this.entity + constraint;
-             window.mx.data.get({
+            //  const constraint = this.props.constraint;
+            const xpath = "//" + this.entity;
+            window.mx.data.get({
                 callback: (objs: mendix.lib.MxObject[]) => {
-                     this.processCheckboxItems(objs);
-                    }
-                     ,
+                    this.processCheckboxItems(objs);
+                }
+                ,
                 xpath,
                 filter: {
                     sort: [ [ this.props.displayAttr, "asc" ] ]
@@ -91,7 +105,7 @@ export default class MultiselectContainer extends Component<ContainerProps, Cont
 
     }
 
-    private processCheckboxItems = (multiSelectObjects: mendix.lib.MxObject[]) => {
+   private processCheckboxItems = (multiSelectObjects: mendix.lib.MxObject[]) => {
         const referencedObjects = this.props.mxObject.getReferences(this.reference);
 
         const checkboxItems = multiSelectObjects.map(mxObj => {
@@ -108,63 +122,86 @@ export default class MultiselectContainer extends Component<ContainerProps, Cont
         this.setState({ checkboxItems, isChecked: true });
     }
 
-    private createCheckboxItems() {
+   private createCheckboxItems() {
         const checkboxarray: any = [];
         this.state.checkboxItems.map(item => {
             checkboxarray.push([
-                        createElement("label", {
-                            className: "myClassName"
-                        }),
-                        createElement("input", {
-                            type: "checkbox",
-                            onChange: this.handelChange,
-                            isChecked: item.isChecked,
-                            key: item.guid
-                        }), item.caption ]
-                    );
-                });
+                createElement("label", {
+                    className: "myClassName"
+                }),
+                createElement("input", {
+                    name: item.caption,
+                    type: "checkbox",
+                    onChange: this.handelChange,
+                    isChecked: item.isChecked,
+                    value: item.guid
+                }), item.caption ]
+            );
+        });
 
         return checkboxarray;
     }
 
-    private setReference = (Node: HTMLDivElement) => {
+   private setReference = (Node: HTMLDivElement) => {
         this.Node = Node;
     }
 
-    private showCheckboxes = () => {
-         const checkboxes = this.Node;
-         if (!expanded) {
-          checkboxes.style.display = "block";
-          expanded = true;
+   private showCheckboxes = () => {
+        const checkboxes = this.Node;
+        if (!expanded) {
+            checkboxes.style.display = "block";
+            expanded = true;
         } else {
-          checkboxes.style.display = "none";
-          expanded = false;
+            checkboxes.style.display = "none";
+            expanded = false;
         }
-      }
+    }
 
-    private handelChange(event: any) {
+   private handelChange = (event: any) => {
         const newEvent = event.target;
         if (newEvent && newEvent.checked) {
-              this.props.mxObject.addReference(this.reference, event.target.value);
-          } else {
-              this.props.mxObject.removeReferences(this.reference, event.target.value);
-          }
-      }
-    private resetSubscriptions(mxObject: mendix.lib.MxObject) {
-          this.subscriptionHandles.forEach(window.mx.data.unsubscribe);
-          this.subscriptionHandles = [];
+            this.props.mxObject.addReference(this.reference, event.target.value);
+            this.myarray.push(newEvent.name);
+        } else {
+            this.props.mxObject.removeReferences(this.reference, event.target.value);
+            this.myarray.splice(this.myarray.indexOf(newEvent.name), 1);
+        }
+        this.setState({ names: this.myarray });
+    }
 
-          this.subscriptionHandles.push(window.mx.data.subscribe({
-              guid: mxObject.getGuid(),
-              attr: this.props.displayAttr,
-              callback: () => this.getDataFromXPath
+   private resetSubscriptions(mxObject: mendix.lib.MxObject) {
+        this.subscriptionHandles.forEach(window.mx.data.unsubscribe);
+        this.subscriptionHandles = [];
 
-                        }),
-                        window.mx.data.subscribe({
-                            entity: this.props.entity1,
-                            callback: () => this.getDataFromXPath
-                            }
-                         )
-                    );
-      }
+        this.subscriptionHandles.push(window.mx.data.subscribe({
+            guid: mxObject.getGuid(),
+            attr: this.props.displayAttr,
+            callback: () => this.getDataFromXPath
+
+        }),
+            window.mx.data.subscribe({
+                entity: this.props.entity1,
+                callback: () => this.getDataFromXPath
+            }
+            )
+        );
+    }
+
+   private getDataFromMicroflow() {
+        const { mxform, dataSourceType } = this.props;
+        if (dataSourceType) {
+            mx.data.action({
+                params: {
+                    applyto: "None",
+                    actionname: dataSourceType
+                },
+                origin: mxform,
+                callback: (mxObject: mendix.lib.MxObject[]) => this.processCheckboxItems(mxObject),
+                error: (error) => {
+                    mx.ui.error(error.message);
+                }
+            });
+        }
+    }
+
 }
